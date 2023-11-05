980fabaf1c18749910ca576bdea093526ad5c122
Bedirhan Deveci <bedirhanyigitdeveci@gmail.com>
2023-11-05 15:05:31 +0100

    main.go was Updated

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ func main() {
 	const conferenceTicket = 50
 	fmt.Println("Welcome to our", name, "booking app")
 	fmt.Println("Get your tickets here to attend")
-
 	fmt.Println("Tickets cost", conferenceTicket, "Euro")
+
+	// now lets add a new line to check from github
+	fmt.Println("This is the new line that has been recently added")
 
 }
