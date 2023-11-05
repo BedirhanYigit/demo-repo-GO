7888033cbd2ff9ce537f0781968d47cb0327cd4a
Bedirhan Deveci <bedirhanyigitdeveci@gmail.com>
2023-11-05 14:57:05 +0100

    go.mod and main.go were added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module firstgoexercise
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Go conference"
+	const conferenceTicket = 50
+	fmt.Println("Welcome to our", name, "booking app")
+	fmt.Println("Get your tickets here to attend")
+
+	fmt.Println("Tickets cost", conferenceTicket, "Euro")
+
+}
